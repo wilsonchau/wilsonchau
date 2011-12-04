e41364e1276e4c84c1e8ff6fe17b1f57c1dcc15d
Wilson Chau <wilsonchau2@gmail.com>
2011-12-03 19:01:34 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wilsonchau::Application.config.secret_token = 'f57fdf2bb5372b8e2fb46eb7ab1becad036a968fdfe0b9d42c205c3ab3e6448bf9e5eeb23fdd76142a905ae6bbff53ee0e5f2432314ac4211096353474ecb7ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wilsonchau::Application.config.session_store :cookie_store, key: '_wilsonchau_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wilsonchau::Application.config.session_store :active_record_store
