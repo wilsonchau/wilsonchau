ef3bd68dce5598a22f2060be6cfcc9cfb0a46561
Wilson Chau <wilsonchau2@gmail.com>
2011-12-07 00:30:20 -0800

    more changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,4 @@
 class HomeController < ApplicationController
   def index
-    title = params[:title]
-    content = params[:content]
-    BlogPost.create(:title => title, :content => content)
-    @all_blog_posts = BlogPost.all
   end
 end
