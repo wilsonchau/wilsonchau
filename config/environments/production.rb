287fb8206653c985e4172f1add85042a801ae69a
Wilson Chau <wilsonchau2@gmail.com>
2011-12-08 00:34:52 -0800

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Wilsonchau::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = trueg
 
   # Generate digests for assets URLs
   config.assets.digest = true
